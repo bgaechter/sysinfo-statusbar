371a8276965824b95b9a74ee16ef1aadfbbaaa1d
bgaechter <benny.gaechter@gmail.com>
2016-08-14 00:16:41 +0000

    print memory and cpu usage in one line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/sysinfo.h>
 #include <iostream>
+#include <string>
 
 float get_memory_usage(long long total_mem, long long used_mem)
 {
@@ -21,16 +22,37 @@ float get_swap_usage(long long total_swap, long long used_swap)
 	return (100*total_swap)/used_swap;
 }
 
+std::string get_bar_chart(float usage)
+{
+	std::string chart{"["};
+	int bars = usage*20/100;
+	for(int i=0; i<22;i++)
+	{
+		if(i<bars)
+		{
+			chart += "|";
+		}
+		else
+		{
+			chart += " ";
+		}
+
+	}
+	chart += "]";
+	return chart;
+}
+
+
 int main()
 {
-	struct sysinfo mem_info;
+	struct sysinfo info;
 
-	sysinfo(&mem_info);
-	long long total_mem = mem_info.totalram;
-	long long total_swap = mem_info.totalswap;
+	sysinfo(&info);
+	long long total_mem = info.totalram;
+	long long total_swap = info.totalswap;
 
-	long long used_mem = mem_info.totalram - mem_info.freeram;
-	long long used_swap = mem_info.totalswap - mem_info.freeswap;
+	long long used_mem = info.totalram - info.freeram;
+	long long used_swap = info.totalswap - info.freeswap;
 
 
 	float memory_usage = get_memory_usage(total_mem, used_mem);
@@ -39,7 +61,14 @@ int main()
 	std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
 	std::cout.precision(2);
 
-	std::cout << "Mem: " << memory_usage << "%" << std::endl;
-	std::cout << "Swap: " << swap_usage << "%" << std::endl;
+	std::cout << "Mem: " << memory_usage << "% ";
+	std::cout << "Swap: " << swap_usage << "% ";
+	// the divison by 65536.0 is kind of a cheat
+	// beacause loads returns the averaged active tasks
+	// see getloadavg.c, uptime.c for details
+	std::cout << info.loads[0]/65536.0 << " ";
+	std::cout << info.loads[1]/65536.0 << " ";
+	std::cout << info.loads[2]/65536.0 << " " << std::endl;
+	
 
 }
